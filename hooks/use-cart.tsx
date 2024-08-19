82b7f9a2231b7491863221ad82fcdbfcf2bb1d72
cjgranda19 <cjgranda1@espe.edu.ec>
2024-08-18 20:59:21 -0500

    remasterized sin back

--- hooks/use-cart.tsx
+++ hooks/use-cart.tsx
@@ -2,18 +2,18 @@ import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
 import { toast } from '@/components/ui/use-toast'
 
-import { ProductType } from "@/types/product"
+import { Product } from "@/types/product"
 
 interface CartStore {
-    items: ProductType[],
-    addItem: (data: ProductType) => void
+    items: Product[],
+    addItem: (data: Product) => void
     removeItem: (id: number) => void
     removeAll: () => void
 }
 
 export const useCart = create(persist<CartStore>((set, get) => ({
     items: [],
-    addItem: (data: ProductType) => {
+    addItem: (data: Product) => {
         const currentItems = get().items
         const existingItem = currentItems.find((item) => item.id === data.id)
 
